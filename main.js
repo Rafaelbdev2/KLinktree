c60706980425afadb52751e83d74811726de8d5e
Rafaelbdev2 <153965815+Rafaelbdev2@users.noreply.github.com>
2024-05-19 22:51:52 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const btn1 = document.querySelector('#button1');
+const btn2 = document.querySelector('#button2');
+const btn3 = document.querySelector('#button3');
+const btn4 = document.querySelector('#button4');
+
+btn1.addEventListener('click', btnOnclick);
+
+function btnOnclick() {
+    window.open("https://www.instagram.com/miss_galvisenglish?igsh=azc1d2NjMXFwbXJj");
+}
+btn2.addEventListener('click', btnOnclick2);
+
+function btnOnclick2() {
+    window.open("https://www.youtube.com/@karinagalviz5249");
+}
+btn3.addEventListener('click', btnOnclick3);
+
+function btnOnclick3() {
+    window.open("https://www.tiktok.com/@miss_galvisteache?_t=8mUtAaAp1ZR&_r=1");
+}
+btn4.addEventListener('click', btnOnclick4);
+
+function btnOnclick4() {
+    window.open("https://www.instagram.com/miss_galvisenglish?igsh=azc1d2NjMXFwbXJj");
+}
+
